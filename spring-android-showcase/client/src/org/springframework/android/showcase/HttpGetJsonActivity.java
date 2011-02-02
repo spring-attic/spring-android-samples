d3737526b402eca8876f62475e2dd176e2c9653a
Roy Clarkson <rclarkson@vmware.com>
2011-02-02 16:36:05 -0500

    Updated license and copyright

--- spring-android-showcase/client/src/org/springframework/android/showcase/HttpGetJsonActivity.java
+++ spring-android-showcase/client/src/org/springframework/android/showcase/HttpGetJsonActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.android.showcase;
 
 import java.util.Arrays;
@@ -13,6 +28,9 @@ import org.springframework.web.client.RestTemplate;
 import android.os.AsyncTask;
 import android.os.Bundle;
 
+/**
+ * @author Roy Clarkson
+ */
 public class HttpGetJsonActivity extends AbstractAsyncListActivity 
 {
 	protected String TAG = "HttpGetActivity";
--- spring-android-showcase/client/src/org/springframework/android/showcase/State.java
+++ spring-android-showcase/client/src/org/springframework/android/showcase/State.java
@@ -1,8 +1,26 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.android.showcase;
 
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
+/**
+ * @author Roy Clarkson
+ */
 @Root(name="state")
 public class State 
 {
