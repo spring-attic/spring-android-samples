f89efe01b96074f9ffb6456df5c1a62b90a02065
Roy Clarkson <rclarkson@vmware.com>
2013-01-03 14:33:34 -0500

    Updated copyright date to 2013

--- spring-android-basic-auth/server/src/test/java/org/springframework/android/basicauth/HomeControllerTest.java
+++ spring-android-basic-auth/server/src/test/java/org/springframework/android/basicauth/HomeControllerTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2011 the original author or authors.
+ * Copyright 2010-2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,25 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package org.springframework.android.basicauth;
-
-import static org.junit.Assert.assertEquals;
-
-import org.junit.Test;
-import org.springframework.android.basicauth.HomeController;
 
 /**
  * @author Roy Clarkson
  */
 public class HomeControllerTest {
-
-    private HomeController controller = new HomeController();
-
-//    @Test
-//    public void homePageSubmitMessage() {
-//        String message = "unit test message";
-//        String response = controller.sendMessage(message);
-//        assertEquals("String message received! Your message: " + message, response);
-//    }
 
 }
