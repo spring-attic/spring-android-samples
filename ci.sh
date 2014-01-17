aab16a0bcfb8be3095baf7b265d36750db00e46f
Roy Clarkson <rclarkson@gopivotal.com>
2014-01-17 17:04:47 -0500

    Update CI script to download and configure the Android environment

--- ci.sh
+++ ci.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
 cd $(dirname $0)
 
+export ANDROID_HOME=${WORKSPACE}/android-sdk-linux 
+export PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}
+wget http://dl.google.com/android/android-sdk_r22.3-linux.tgz -nv
+tar xzf android-sdk_r22.3-linux.tgz
+echo "y" | android update sdk -f -u -a -t tools,platform-tools,build-tools-19.0.1,build-tools-19.0.0,build-tools-18.1.1,build-tools-18.1.0,build-tools-18.0.1,build-tools-17.0.0,android-17,extra-android-m2repository,extra-android-support
+
 function build {
     ./$1/ci.sh
     ret=$?
