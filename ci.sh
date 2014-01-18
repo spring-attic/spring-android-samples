4836e35cee80e8478b668590f7877fbc410809db
Roy Clarkson <rclarkson@gopivotal.com>
2014-01-17 23:19:24 -0500

    Update CI script

--- ci.sh
+++ ci.sh
@@ -1,5 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 cd $(dirname $0)
+
+if [ ! -d "$ANDROID_HOME" ]; then
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        export ANDROID_HOME=${WORKSPACE}/android-sdk-linux 
+        export PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}
+        wget http://dl.google.com/android/android-sdk_r22.3-linux.tgz -nv
+        tar xzf android-sdk_r22.3-linux.tgz
+        echo "y" | android update sdk -n -f -u -a -t tools,platform-tools,build-tools-19.0.1,build-tools-19.0.0,build-tools-18.1.1,build-tools-18.1.0,build-tools-18.0.1,build-tools-17.0.0,android-17,extra-android-m2repository,extra-android-support
+    #elif [[ "$OSTYPE" == "darwin"* ]]; then
+    else
+        echo "ANDROID_HOME is not available"
+        exit
+    fi
+fi
 
 function build {
     ./$1/ci.sh
