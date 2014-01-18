aad9641dfa2b66258424cc3184ccb019b06e589c
Roy Clarkson <rclarkson@gopivotal.com>
2014-01-17 23:37:52 -0500

    Update CI script

--- ci.sh
+++ ci.sh
@@ -7,7 +7,7 @@ if [ ! -d "$ANDROID_HOME" ]; then
         export PATH=${ANDROID_HOME}/tools:${ANDROID_HOME}/platform-tools:${PATH}
         wget http://dl.google.com/android/android-sdk_r22.3-linux.tgz -nv
         tar xzf android-sdk_r22.3-linux.tgz
-        echo "y" | android update sdk -n -f -u -a -t tools,platform-tools,build-tools-19.0.1,build-tools-19.0.0,build-tools-18.1.1,build-tools-18.1.0,build-tools-18.0.1,build-tools-17.0.0,android-17,extra-android-m2repository,extra-android-support
+        echo "y" | android update sdk -f -u -a -t tools,platform-tools,build-tools-19.0.1,build-tools-19.0.0,build-tools-18.1.1,build-tools-18.1.0,build-tools-18.0.1,build-tools-17.0.0,android-17,extra-android-m2repository,extra-android-support
     #elif [[ "$OSTYPE" == "darwin"* ]]; then
     else
         echo "ANDROID_HOME is not available"
